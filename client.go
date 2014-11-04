6b1e2c118faedd72c1d9bb50494786cd3f320f41
Erlend Røsjø <erlend.rosjo@knowit.no>
2014-11-05 00:12:04 +0100

    Added basic message queue

---
+++ client.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	var filename = "45726c656e642d526f736a6f2e6a7067e691a4d41d8cd98f00b204e9800998ecf8427e.jpg"
+	body := filename
+	err = ch.Publish(
+		"",           // exchange
+		"task_queue", // routing key
+		false,        // mandatory
+		false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/plain",
+			Body:         []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+}
--- resizer.go
+++ resizer.go
@@ -4,19 +4,82 @@ import (
 	"bytes"
 	"github.com/erlendr/store"
 	"github.com/nfnt/resize"
+	"github.com/streadway/amqp"
 	"image/jpeg"
 	"io"
+	"log"
+	"time"
 )
 
 func main() {
-	var filename = "45726c656e642d526f736a6f2e6a7067e691a4d41d8cd98f00b204e9800998ecf8427e.jpg"
-	rc := store.Download(filename)
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
 
-	println("Resizer - File " + filename + " downloaded")
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
 
-	var buf = resizeImage(rc, 100)
-	var reader = bytes.NewReader(buf.Bytes())
-	store.UploadReader("thumb.jpg", reader, int64(reader.Len()))
+	q, err := ch.QueueDeclare(
+		"task_queue", // name
+		true,         // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ch.Qos(
+		3,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+
+	forever := make(chan bool)
+
+	go func() {
+		println("Waiting for messages...")
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			d.Ack(false)
+
+			var filename = string(d.Body)
+			rc := store.Download(filename)
+
+			println("Resizer - File " + filename + " downloaded")
+
+			var buf = resizeImage(rc, 100)
+			var reader = bytes.NewReader(buf.Bytes())
+			store.UploadReader("thumb.jpg", reader, int64(reader.Len()))
+
+			dot_count := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dot_count)
+			time.Sleep(t * time.Second)
+		}
+	}()
+
+	<-forever
+
 }
 
 func resizeImage(file io.Reader, width uint) *bytes.Buffer {
